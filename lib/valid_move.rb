36b1e9d0c9738c1d21331ff61594a697f41ea46f
A I <atbisrael@gmail.com>
2018-01-08 02:32:52 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@ def valid_move?(board, index)
   if (position_taken?(board, index || board != [" ", " ", " ", " ", " ", " ", " ", " ", " "])
     return false 
   end
-  if (board != [" ", " ", " ", " ", " ", " ", " ", " ", " "] && position_taken?(board, index)
+  if (board != [" ", " ", " ", " ", " ", " ", " ", " ", " "] && position_taken?(board, index))
     return true 
   end
  
