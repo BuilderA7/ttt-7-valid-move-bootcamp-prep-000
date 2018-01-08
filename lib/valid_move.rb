581124b4ec0f3948ba85d048e39319c1e731c218
A I <atbisrael@gmail.com>
2018-01-08 02:48:11 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
+  
    if (board == [" ", " ", " ", " ", " ", " ", " ", " ", " "] && position_taken?(board, index))
      #&& index <= board.length && index > 0 )
     return true 
