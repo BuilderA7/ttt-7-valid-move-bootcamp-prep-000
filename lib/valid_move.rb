98c27a7f7602d5f5ee0f746d448122a0fef30a4b
A I <atbisrael@gmail.com>
2018-01-08 01:39:00 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,14 +1,15 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
+   if (board[index] != [" "])
+    return false 
+  end
   if (board == [" ", " ", " ", " ", " ", " ", " ", " ", " "])
     return true 
   end
   if (board != [" ", " ", " ", " ", " ", " ", " ", " ", " "])
     return true 
   end
-  if (board[index] != [" "] && )
-    return false 
-  end
+ 
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
